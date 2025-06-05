1d8006ab2dd8afc8e3aa85a629b3659ebdcfb4b8
Tildeneitor <tomasgonzalezcontre@gmail.com>
2025-06-05 13:23:19 -0400

    updateBACKEND

---
+++ dbbarcheck/core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Administrador, Alcohol, ListaDeAlcohol, Barra, Reporte
+
+admin.site.register(Administrador)
+admin.site.register(Alcohol)
+admin.site.register(ListaDeAlcohol)
+admin.site.register(Barra)
+admin.site.register(Reporte)
